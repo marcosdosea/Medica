043d57498f7924843a836fffc86634f774e774ff
Guilherme Lima Santos <grguilh2rm2lima@gmail.com>
2026-05-30 16:33:25 -0300

    add TipoAlergia

---
+++ Codigo/Web/Core/Enum/Alergia/TipoAlergia.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Enum.Alergia
+{
+    public enum TipoAlergia
+    {
+        [Display(Name = "Medicamentosa")]
+        MEDICAMENTO,
+
+        [Display(Name = "Alimentar")]
+        ALIMENTAR,
+
+        [Display(Name = "Respiratória")]
+        RESPIRATORIA,
+
+        [Display(Name = "Por Contato")]
+        CONTATO,
+
+        [Display(Name = "Outros")]
+        OUTROS
+    }
+}
